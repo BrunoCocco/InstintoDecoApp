6a07b3258d370fc371d582ed757d70a5cf2c5b9f
Bruno Cocco <brunoe.cocco@gmail.com>
2022-09-17 21:09:11 -0300

    agregando toastify

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
-// validador de contraseñas
-let nombre = prompt ( "ingrese su nombre")
-let verificarPassword = prompt ( " ingrese el password")
+//TODO: agregar toastify. 
+const confirmModalLogin = document.getElementById("Boton__id--confirm");
+confirmModalLogin.addEventListener("click", ()=>{
+    Toastify({
 
-let User = [nombre , verificarPassword]
+        text: "This is a toast",
+        
+        duration: 3000
+        
+        }).showToast();
+})
 
-
-//constructor
-class crearUsuario {
-    constructor( nombre, password ){
-    this.nombre= nombre
-    this.password=password
-    }
+const botonEnviarEncuesta = document.getElementById("Boton__enviar--encuesta");
+botonEnviarEncuesta.addEventListener("click", ()=>{
+//FIXME: tomar los valores de los imputs
+    //let formNameUser = document.getElementById("form--name__user").value
+    //console.log(formNameUser)
+    // let formNameHambiente = document.getElementById("form--name__hambiente").value
+    // let formNameColor = document.getElementById("form--name__color").value
+    // let formNameDistribuciones = document.getElementById("form--name__distribuciones").value
+    // let formNameAperturas = document.getElementById("form--name__aperturas").value
+    // console.log(botonEnviarEncuesta)
+//FIXME: modificar el toastify    
+    Toastify({
+        text: "✔️ El Formulario se envio Correctamente!",
+        duration: 2000
+        }).showToast();
     
-//verifica pass
-    verificarPass (pass){
-    if (pass === this.password){
-    alert ("bienvenido")}
-    else{
-        alert ("Acceso Denegado")
-    }
-    }
-    }
-    //crea usuario
-    let newUser = new crearUsuario ( nombre, "1234")
-    newUser.verificarPass(verificarPassword)
-    
-
+})
